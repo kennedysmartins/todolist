3b9241098f887671470d3cf4e625f245edbecc71
Kennedy Martins <kennedysmartins@hotmail.com>
2024-05-21 17:00:44 -0300

    styles main page

---
+++ app/styles.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 1rem;
+  margin: 5rem auto;
+  width: 110rem;
+  max-width: 100%; 
+  padding: 0 1.5rem;
+`;
+
+export const Title = styled.h1`
+  text-transform: uppercase;
+  font-weight: 900;
+  text-align: center;
+`;
