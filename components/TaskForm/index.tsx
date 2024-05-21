d1d22f80a8ecf173807ef4bd6c06b734c9873555
Kennedy Martins <kennedysmartins@hotmail.com>
2024-05-21 17:01:12 -0300

    Page and style TaskForm component

---
+++ components/TaskForm/index.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import * as C from "./styles";
+
+interface TaskFormProps {
+  onSubmit: (values: TaskValues, actions: any) => void;
+}
+
+interface TaskValues {
+  task: string;
+}
+
+const TaskForm: React.FC<TaskFormProps> = ({ onSubmit }) => {
+  const initialValues: TaskValues = { task: "" };
+
+  const validationSchema = Yup.object({
+    task: Yup.string().required(),
+  });
+
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={onSubmit}
+    >
+      {() => (
+          <C.Main>
+        <Form>
+            <C.Container>
+            <C.Button type="submit">Nova</C.Button>
+              <C.StyledField
+                name="task"
+                type="text"
+                placeholder="Escreva aqui..."
+                autoComplete="off"
+              />
+
+            </C.Container>
+        </Form>
+          </C.Main>
+      )}
+    </Formik>
+  );
+};
+
+export default TaskForm;
