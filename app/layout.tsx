b6fd18789b87e0426c862d37b09dcc18c2f68d6b
Kennedy Martins <kennedysmartins@hotmail.com>
2024-05-23 02:26:00 -0300

    change title and add Header on layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,12 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Desafio Hub - Frontend"
+  title: "Desafio Hubbi - Frontend"
 };
 
 export default function RootLayout({
@@ -15,7 +16,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <Header />
+        {children}
+        </body>
     </html>
   );
 }
