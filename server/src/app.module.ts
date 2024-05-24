81a8b4b10baaa85eb69243fcc0536849f7ebb238
Kennedy Martins <kennedysmartins@hotmail.com>
2024-05-24 01:16:15 -0300

    Added TaskModule

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksService } from './tasks/tasks.service';
 import { TasksController } from './tasks/tasks.controller';
 import { PrismaService } from './database/prisma.service';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, TasksController],
+  imports: [TasksModule],
+  controllers: [TasksController],
   providers: [AppService, TasksService, PrismaService],
 })
 export class AppModule {}
