b1bb5feb4afb720a7502a6619894e9cb3503f724
Kennedy Martins <kennedysmartins@hotmail.com>
2024-05-24 01:17:01 -0300

    change to interface

--- server/src/task.model.ts
+++ server/src/task.model.ts
@@ -1,9 +1,9 @@
 // src/task.model.ts
 
-export class Task {
-    id: string;
+export interface Task {
+    id?: string;
     title: string;
-    description: string;
+    description?: string;
     completed: boolean;
     createdAt: Date;
     updatedAt: Date;
